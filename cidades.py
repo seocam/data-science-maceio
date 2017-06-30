6b77308825dad305a8a2973571a03c5ed10e0411
Sergio Oliveira <seocam@seocam.com>
2017-06-30 14:30:49 -0300

    Descompacta arquivo de cidades

--- cidades.py
+++ cidades.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
 
 import urllib.request
+import zipfile
+
 
 url = 'http://download.geonames.org/export/zip/BR.zip'
 zip_path = '/tmp/BR.zip'
+unzip_path = '/tmp/cidades/'
 
 # Download das cidades brasileiras
 print('Baixando cidades brasileiras (BR.zip)')
 urllib.request.urlretrieve(url, zip_path)
+
+# Descompacta zip das cidades
+with zipfile.ZipFile(zip_path) as cidades_zip:
+    print('Descompactando arquivo de cidades')
+    cidades_zip.extractall(unzip_path)
