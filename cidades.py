a7aa77bb401050163cc3ab2e109db879e37769e3
Sergio Oliveira <seocam@seocam.com>
2017-06-30 14:27:54 -0300

    Download das cidades brasileiras do geonames

--- cidades.py
+++ cidades.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python
+
+import urllib.request
+
+url = 'http://download.geonames.org/export/zip/BR.zip'
+zip_path = '/tmp/BR.zip'
+
+# Download das cidades brasileiras
+print('Baixando cidades brasileiras (BR.zip)')
+urllib.request.urlretrieve(url, zip_path)
