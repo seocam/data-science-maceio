37b67f5a274198612236dd3ba914fa10d1a61892
Sergio Oliveira <seocam@seocam.com>
2017-06-30 14:44:55 -0300

    Cria banco de dados SQL

--- cidades.py
+++ cidades.py
@@ -3,12 +3,14 @@
 import csv
 import os
 import urllib.request
+import sqlite3
 import zipfile
 
 
 url = 'http://download.geonames.org/export/zip/BR.zip'
 zip_path = '/tmp/BR.zip'
 unzip_path = '/tmp/cidades/'
+cidades_db = '/tmp/cidades.db'
 
 if not os.path.exists(zip_path):
     # Download das cidades brasileiras
@@ -21,6 +23,11 @@ else:
 with zipfile.ZipFile(zip_path) as cidades_zip:
     print('Descompactando arquivo de cidades')
     cidades_zip.extractall(unzip_path)
+
+# Cria o bando de dados
+conn = sqlite3.connect(cidades_db)
+conn.execute('CREATE TABLE cidades (cep text, nome text, estado text);')
+conn.commit()
 
 # Le arquivo CSV
 with open(unzip_path + 'BR.txt') as cidades_file:
